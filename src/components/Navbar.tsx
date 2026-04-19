8d2700bb00def99c76e20c7e7543d1890e2b9581
keerthivasan.b1-v <keerthivasan.b1-v@abfrl.adityabirla.com>
2026-04-19 19:34:43 +0530

    Chnages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -415,7 +415,9 @@ export default function Navbar() {
                     transition: "all 0.2s",
                   }}
                 >
-                  <span className="navbar-login-text">Profile</span>
+                  <span className="navbar-login-text">
+                    {profileData?.firstName || ""}
+                  </span>
                   <div className="profileWrapper">
                     {profileData?.firstName?.[0]?.toUpperCase()}
                   </div>
